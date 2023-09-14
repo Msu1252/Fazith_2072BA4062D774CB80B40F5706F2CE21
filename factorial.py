8db9632952f34dbe85299e0ca40183197d70e238
Msu1252 <144984506+Msu1252@users.noreply.github.com>
2023-09-14 10:58:23 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,7 @@
+Enter a value : 
+5
+The factorial of 5 is 120.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
---
+++ leapyear (1).py
@@ -0,0 +1,7 @@
+Enter a year : 
+2002
+2002 is not a leap year.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
